3f300c03cbd1068fd119866ef6ca2f769dcfca64
Mario Jimenez <mariojose.0732@gmail.com>
2020-05-11 17:20:39 -0400

    Updates

---
+++ precache-manifest.ac2444be81f67a6a48a9ad83fc5c7194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d4e93de3ff028eb810b5d0d810cb0a",
+    "url": "/kanban-board-react/index.html"
+  },
+  {
+    "revision": "b6de21fd54ff34f7218a",
+    "url": "/kanban-board-react/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "b6de21fd54ff34f7218a",
+    "url": "/kanban-board-react/static/js/2.b3aa2d64.chunk.js"
+  },
+  {
+    "revision": "b3e4cdce8e3a1dbb946c17e2e81d27cf",
+    "url": "/kanban-board-react/static/js/2.b3aa2d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f238a2af50b09eecc86",
+    "url": "/kanban-board-react/static/js/main.78f59945.chunk.js"
+  },
+  {
+    "revision": "0d5589d90b7f48066cfe",
+    "url": "/kanban-board-react/static/js/runtime-main.3b2ee96d.js"
+  }
+]);
